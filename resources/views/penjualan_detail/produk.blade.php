60587d0e9392a395c82b786e4ca174d96acfcb1f
Miku217500 <rafaelfarizi1@gmail.com>
2023-05-23 14:04:34 +0700

    change to harga beli

--- resources/views/penjualan_detail/produk.blade.php
+++ resources/views/penjualan_detail/produk.blade.php
@@ -13,7 +13,7 @@
                         <th width="5%">No</th>
                         <th>Kode</th>
                         <th>Nama</th>
-                        <th>Harga</th>
+                        <th>Harga Pokok</th>
                         <th><i class="fa fa-cog"></i></th>
                     </thead>
                     <tbody>
@@ -22,7 +22,7 @@
                                 <td width="5%">{{ $key+1 }}</td>
                                 <td><span class="label label-success">{{ $item->kode_produk }}</span></td>
                                 <td>{{ $item->nama_produk }}</td>
-                                <td>{{ $item->harga_jual }}</td>
+                                <td>{{ $item->harga_beli }}</td>
                                 <td>
                                     <a href="#" class="btn btn-primary btn-xs btn-flat"
                                         onclick="pilihProduk('{{ $item->id_produk }}', '{{ $item->kode_produk }}')">
